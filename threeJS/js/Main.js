368793056d5e089d60828eeb638ce9b068717eec
villain2 <djvillain@hotmail.com>
2014-01-16 12:06:28 -0500

    fixing js

--- threeJS/js/Main.js
+++ threeJS/js/Main.js
@@ -1,6 +1,7 @@
 var width               = window.innerWidth;
 var height              = window.innerHeight;
 
+
 var renderer            = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(width, height);
 document.body.appendChild(renderer.domElement);
