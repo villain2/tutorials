6f36010aad3e9f157daa6596677e0c26e6820281
villain2 <djvillain@hotmail.com>
2014-01-21 13:06:29 -0500

    new samples

--- box2dJS/app/js/controllers/VirtualShapes.controllers.js
+++ box2dJS/app/js/controllers/VirtualShapes.controllers.js
@@ -8,8 +8,9 @@
 		scope.type 				= $attrs.type;
 		scope.instanceID 		= parseInt($attrs.instanceid);
 
-		//initiate box2d
+		//initiate box2d and get libraries and variables
 		var b2Vec2				= Box2D.Common.Math.b2Vec2;
-		console.log(b2Vec2);
+		var world				= new b2World(new b2Vec2(0, 10), true);
+
 	}
 }(VirtualShapes.Controllers = VirtualShapes.Controllers || {} ));
